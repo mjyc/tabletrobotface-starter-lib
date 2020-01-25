16df583c9f6a354564f7dc92e914cf9eca9991ad
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-01-25 09:12:09 -0800

    WIP: simplify

--- src/index.js
+++ src/index.js
@@ -11,26 +11,15 @@ import {
 } from "@cycle-robot-drivers/run";
 import { makeTabletFaceDriver } from "@cycle-robot-drivers/screen";
 import {
-  extractFaceFeatures,
   mockDownloadDataSource,
   makeDownloadDataDriver,
   makeMediaRecorderDriver,
   mockMediaRecorderSource,
   DataDownloader
 } from "tabletrobotface-userstudy";
-import streams from "listrp/streams";
-import { fromXStream, toXStream } from "listrp/cyclebridge";
 import makeAudioAverageFrequencyDriver from "./makeAudioAverageFrequencyDriver";
 import { makePoseDetectionDriver } from "cycle-posenet-driver";
-import programs from "iterrep/programs";
-import {
-  evaluateParams,
-  repair,
-  deriveExpectedStateTrace
-} from "iterrep/repair";
-import repairOptss from "iterrep/testdata/defaultRepairOptss";
 
-const parameters = require("./parameters.json");
 const settings = require("./settings.json");
 const {
   userstudy,
@@ -62,250 +51,29 @@ const convertTimeTravelRecordedStreamToRecordedStream = timeTravelRecorded => {
 };
 
 const makeUserstudy = (plan, Time) => {
-  // parse args
-  const { iter = 0, results = [] } =
-    JSON.parse(localStorage.getItem("iterrep_repair")) || {};
-  const name = plan[iter].name;
-  const progParams =
-    iter === 0 || name !== results[iter - 1].name
-      ? parameters[name]
-      : results[iter - 1].repairOutput.progParams; // should update
-  const prior =
-    iter === 0 || name !== results[iter - 1].name
-      ? []
-      : results[iter - 1].repairOutput.posterior;
-  const instIndex = plan[iter].instIndex;
-  console.warn("iter", iter);
-  console.warn("progParams", JSON.stringify(progParams, null, 2));
-  console.warn("instIndex", instIndex);
-  console.warn("repairOptss[name]", JSON.stringify(repairOptss[name], null, 2));
-
   // create a program
-  const progOptions = {
-    instructions: programs.instructions[name][instIndex],
-    startImmediately: false,
-    mode: "book", // for "story"
-    outputLabels: true,
-    ss: Object.assign({}, streams, {
-      sdelay: (period, stream) => {
-        const in$ = toXStream(stream);
-        return fromXStream(in$.compose(Time.delay(period)));
-      },
-      sthrottle: (period, stream) => {
-        const in$ = toXStream(stream);
-        return fromXStream(in$.compose(Time.throttle(period)));
-      },
-      sdebounce: (fn, stream) => {
-        const in$ = toXStream(stream);
-        return fromXStream(
-          in$.map(x => xs.of(x).compose(Time.delay(fn(x)))).flatten()
-        );
-      },
-      sbuffer: (windowSizeMs, stream) => {
-        return cb => {
-          const buffer = [];
-          return stream(payload => {
-            const stamp = Time._time();
-            buffer.push({ stamp, value: payload });
-            while (buffer[0].stamp < stamp - windowSizeMs) {
-              buffer.shift();
-            }
-            cb(buffer.map(x => x.value));
-          });
-        };
-      }
-    })
+  // const createProgram = programs[name];
+  // const repairOpts = repairOptss[name];
+  // const program = createProgram(progParams, progOptions);
+
+  const program = sources => {
+    console.log("started program!", sources);
+    const sinks = {
+      setMessage: xs.of("hello"),
+      askMultipleChoice: xs.of(["hello", "b"])
+    };
+    return sinks;
   };
-  const createProgram = programs[name];
-  const repairOpts = repairOptss[name];
-  const program = createProgram(progParams, progOptions);
 
   // prepare running the program
-  const setMessagePush$ = xs.create();
-  const askMultipleChoicePush$ = xs.create();
   return sources => {
-    xs.combine(sources.recordedStreams, sources.askMultipleChoiceFinished)
-      .filter(x => x[1] === "What's next?")
-      .take(1)
-      .compose(Time.delay(100)) // delay to display "Please answer..."
-      .addListener({
-        next: async ([recordedStreams, _]) => {
-          // prepare repair inputs
-          const traces = {};
-          for (const k of Object.keys(recordedStreams)) {
-            traces[k] = convertTimeTravelRecordedStreamToRecordedStream(
-              recordedStreams[k]
-            );
-          }
-          // adjust timestamps
-          const startStamp = traces.facePoses[0].stamp;
-          for (const k of Object.keys(traces)) {
-            traces[k] = traces[k]
-              .filter(x => x.stamp >= startStamp)
-              .map(x => {
-                return {
-                  value: x.value,
-                  stamp: x.stamp - startStamp
-                };
-              });
-          }
-          const inputTraces = {
-            tabletfaceLoaded: traces.tabletfaceLoaded,
-            facePoses: traces.facePoses,
-            voiceLevel: traces.voiceLevel,
-            askMultipleChoiceFinished: traces.askMultipleChoiceFinished,
-            sayFinished: traces.sayFinished
-          };
-          const stateTrace = deriveExpectedStateTrace(traces);
-          console.debug("derived stateTrace", stateTrace);
+    const programSinks = program(sources);
 
-          // stop recording
-          !record && stopRecording$.shamefullySendNext(xs.never());
-
-          // evaluate current params
-          const evalOut = await evaluateParams({
-            createProgram,
-            progParams,
-            inputTraces,
-            stateTrace,
-            options: {
-              computeOverlapBinSize: 100
-            }
-          });
-
-          // repair
-          const repairOutput = await repair({
-            createProgram,
-            progParams,
-            inputTraces,
-            stateTrace,
-            options: Object.assign({ prior }, repairOpts)
-          });
-          const { score: repairedScore } = await evaluateParams({
-            createProgram,
-            progParams: repairOutput.progParams,
-            inputTraces,
-            stateTrace,
-            options: {
-              computeOverlapBinSize: 100
-            }
-          });
-          console.debug("finished repair");
-          console.debug("evalOut.score", evalOut.score);
-          console.debug("repairedScore", repairedScore);
-          console.debug("repairOutput.progParams", repairOutput.progParams);
-
-          // store results
-          const outputTraces = {
-            setMessage: traces.setMessage,
-            setImage: traces.setImage,
-            askMultipleChoice: traces.askMultipleChoice,
-            say: traces.say,
-            detector: traces.detector,
-            state: traces.state,
-            videoStart: traces.videoStart,
-            TabletFace: traces.TabletFace
-          };
-          results[iter] = {
-            iter,
-            name,
-            evalOut,
-            repairInputs: {
-              createProgram,
-              progParams,
-              inputTraces,
-              stateTrace,
-              repairOpts
-            },
-            outputTraces,
-            repairOutput,
-            repairedScore
-          };
-          const data = {
-            iter: iter + 1,
-            results
-          };
-          localStorage.setItem("iterrep_repair", JSON.stringify(data));
-          console.debug("updated localStorage.iterrep_repair");
-          // console.debug(localStorage.getItem("iterrep_repair"));
-
-          setMessagePush$.shamefullySendNext(
-            "Please answer the questions in the main page"
-          );
-          if (iter !== plan.length - 1) {
-            document.querySelector(".download").innerText = "Download & Reload";
-            setTimeout(() => {
-              askMultipleChoicePush$.shamefullySendNext([
-                "Done! Let's move on"
-              ]);
-            }, 5000);
-            sources.askMultipleChoiceFinished
-              .filter(x => x === "Done! Let's move on")
-              .addListener({
-                next: x => {
-                  location.reload();
-                  setMessagePush$.shamefullySendNext("Thanks! Moving on...");
-                  askMultipleChoicePush$.shamefullySendNext([]);
-                }
-              });
-          } else {
-            document.querySelector(".download").innerText = "Download & Finish";
-          }
-          document.querySelector(".download").onclick = () => {
-            if (iter === plan.length - 1) {
-              console.debug("downloading data");
-              const el = document.createElement("a");
-              el.id = "download_iterrep_repair";
-              el.href = window.URL.createObjectURL(
-                new Blob([localStorage.getItem("iterrep_repair")], {
-                  type: "application/json"
-                })
-              );
-              el.download = `robot_study_${name}`;
-              el.click();
-
-              if (!disableCleanUp) {
-                console.debug("removing item from localStorage");
-                localStorage.removeItem("iterrep_repair");
-              }
-            }
-          };
-          document.querySelector(".download").click();
-        }
-      });
-
-    const programSinks = program({
-      tabletfaceLoaded: fromXStream(sources.tabletfaceLoaded),
-      facePoses: fromXStream(sources.facePoses),
-      voiceLevel: fromXStream(sources.voiceLevel),
-      askMultipleChoiceFinished: fromXStream(sources.askMultipleChoiceFinished),
-      sayFinished: fromXStream(sources.sayFinished)
-    });
-
-    // for better UX
-    const setMessage$ = sources.askMultipleChoiceFinished
-      .filter(x => x === "What's next?")
-      .take(1)
-      .mapTo(
-        iter !== plan.length - 1
-          ? "Please answer the questions in the main page (Finishing up...)"
-          : "Please answer the questions and upload the downloaded file in the main page (Finishing up...)"
-      );
-    const askMultipleChoice$ = sources.askMultipleChoiceFinished
-      .filter(x => x === "What's next?")
-      .take(1)
-      .mapTo([]);
-
-    const sinks = Object.keys(programSinks).reduce((prev, k) => {
-      prev[k] = toXStream(programSinks[k]);
-      return prev;
-    }, {});
-    sinks.setMessage = xs.merge(sinks.setMessage, setMessage$, setMessagePush$);
-    sinks.askMultipleChoice = xs.merge(
-      sinks.askMultipleChoice,
-      askMultipleChoice$,
-      askMultipleChoicePush$
-    );
+    // const sinks = Object.keys(programSinks).reduce((prev, k) => {
+    //   prev[k] = toXStream(programSinks[k]);
+    //   return prev;
+    // }, {});
+    const sinks = programSinks;
     return sinks;
   };
 };
@@ -330,7 +98,7 @@ const main = sources => {
   const sinks = withState(
     withTabletFaceRobotActions(
       sources => {
-        // inputs
+        // Inputs
         const time$ = sources.Time.animationFrames()
           .compose(sources.Time.throttle(100))
           .map(({ time }) => time);
@@ -343,21 +111,21 @@ const main = sources => {
           .compose(sampleCombine(sources.VoiceLevel))
           .map(x => x[1]);
 
-        printStatus &&
-          facePoses$
-            .map(extractFaceFeatures)
-            .compose(sampleCombine(voiceLevel$))
-            .addListener({
-              next: x =>
-                console.debug(
-                  "voiceLevel",
-                  x[1].toFixed(2),
-                  "faceAngle",
-                  x[0].faceAngle.toFixed(2),
-                  "noseAngle",
-                  x[0].noseAngle.toFixed(2)
-                )
-            });
+        // printStatus &&
+        //   facePoses$
+        //     .map(extractFaceFeatures)
+        //     .compose(sampleCombine(voiceLevel$))
+        //     .addListener({
+        //       next: x =>
+        //         console.debug(
+        //           "voiceLevel",
+        //           x[1].toFixed(2),
+        //           "faceAngle",
+        //           x[0].faceAngle.toFixed(2),
+        //           "noseAngle",
+        //           x[0].noseAngle.toFixed(2)
+        //         )
+        //     });
 
         // throttling askMultipleChoiceFinished$ makes buttons to flicker
         const askMultipleChoiceFinished$ = xs
