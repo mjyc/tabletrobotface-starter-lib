5fae06c1ef6796b7f69f1808c4f8715788120121
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-01-25 09:54:04 -0800

    WIP: simplify

--- src/index.js
+++ src/index.js
@@ -20,28 +20,29 @@ import {
 import makeAudioAverageFrequencyDriver from "./makeAudioAverageFrequencyDriver";
 import { makePoseDetectionDriver } from "cycle-posenet-driver";
 
+// ------
+// Consts
+// ------
+
 const settings = require("./settings.json");
-const {
-  userstudy,
-  record = false,
-  displayPoseViz = false,
-  printStatus = false,
-  disableCleanUp = false,
-  hideScroll = true
-} = settings;
+// defaults to "dev" setting
+const { record = true, displayPoseViz = true, hideScroll = false } = settings;
 if (hideScroll) {
   document.body.style.overflow = "hidden";
 }
-console.warn("record", record);
-console.warn("userstudy", JSON.stringify(userstudy, null, 2));
 
 const videoWidth = 640;
 const videoHeight = 480;
 
-const stopRecording$ = xs.create();
+const stopRecording$ = xs.create(); // used for stopping recording
+// stop recording in 30min
 setTimeout(() => {
   stopRecording$.shamefullySendNext(xs.never());
-}, 60 * 30000); // stop recording in 30min
+}, 60 * 30000);
+
+// ---------
+// Functions
+// ---------
 
 const convertTimeTravelRecordedStreamToRecordedStream = timeTravelRecorded => {
   return timeTravelRecorded.map(x => ({
@@ -50,36 +51,26 @@ const convertTimeTravelRecordedStreamToRecordedStream = timeTravelRecorded => {
   }));
 };
 
-const makeUserstudy = (plan, Time) => {
-  // create a program
-  // const createProgram = programs[name];
-  // const repairOpts = repairOptss[name];
-  // const program = createProgram(progParams, progOptions);
-
+const makeProgram = ({ Time = null } = {}) => {
+  // an example program
   const program = sources => {
-    console.log("started program!", sources);
     const sinks = {
-      setMessage: xs.of("hello"),
-      askMultipleChoice: xs.of(["hello", "b"])
+      setMessage: xs.of("Hello!"),
+      askMultipleChoice: xs.of(["Hi"])
     };
     return sinks;
   };
 
-  // prepare running the program
   return sources => {
+    const programSources = sources;
     const programSinks = program(sources);
-
-    // const sinks = Object.keys(programSinks).reduce((prev, k) => {
-    //   prev[k] = toXStream(programSinks[k]);
-    //   return prev;
-    // }, {});
     const sinks = programSinks;
     return sinks;
   };
 };
 
 const main = sources => {
-  // HACK! disable the download button if record is false
+  // disable the download button if record is false
   if (!record) {
     sources.DOM.element(".download")
       .take(1)
@@ -95,234 +86,206 @@ const main = sources => {
 
   const dataProxy$ = xs.create(); // for recording data
 
-  const sinks = withState(
-    withTabletFaceRobotActions(
-      sources => {
-        // Inputs
-        const time$ = sources.Time.animationFrames()
-          .compose(sources.Time.throttle(100))
-          .map(({ time }) => time);
-
-        const tabletfaceLoaded$ = sources.TabletFace.events("load");
-        const facePoses$ = time$
-          .compose(sampleCombine(sources.PoseDetection.events("poses")))
-          .map(x => x[1]);
-        const voiceLevel$ = time$
-          .compose(sampleCombine(sources.VoiceLevel))
-          .map(x => x[1]);
-
-        // printStatus &&
-        //   facePoses$
-        //     .map(extractFaceFeatures)
-        //     .compose(sampleCombine(voiceLevel$))
-        //     .addListener({
-        //       next: x =>
-        //         console.debug(
-        //           "voiceLevel",
-        //           x[1].toFixed(2),
-        //           "faceAngle",
-        //           x[0].faceAngle.toFixed(2),
-        //           "noseAngle",
-        //           x[0].noseAngle.toFixed(2)
-        //         )
-        //     });
-
-        // throttling askMultipleChoiceFinished$ makes buttons to flicker
-        const askMultipleChoiceFinished$ = xs
-          .create({
-            start: listener =>
-              sources.HumanSpeechbubbleAction.result.addListener(listener),
-            stop: () => {}
-          })
-          .map(r => r.result);
-        const sayFinished$ = xs
-          .create({
-            start: listener =>
-              sources.SpeechSynthesisAction.result.addListener(listener),
-            stop: () => {}
-          })
-          .map(r => r.result.text);
-
-        // create a repairable program
-        const sinks = makeUserstudy(userstudy, sources.Time)({
-          recordedStreams: dataProxy$,
-          tabletfaceLoaded: tabletfaceLoaded$,
-          facePoses: facePoses$,
-          voiceLevel: voiceLevel$,
-          askMultipleChoiceFinished: askMultipleChoiceFinished$,
-          sayFinished: sayFinished$
-        });
-
-        const followFace$ = sinks.followFace || xs.never();
-        const express$ = sinks.express || xs.never();
-        const tabletFace$ = xs.merge(
-          sources.TabletFace.events("load").mapTo({
-            type: "START_BLINKING",
-            value: { maxInterval: 10000 }
-          }),
-          xs.combine(followFace$, facePoses$).map(([detecting, poses]) => {
-            if (
-              !!detecting &&
-              poses.length > 0 &&
-              poses[0].keypoints.filter(kpt => kpt.part === "nose").length > 0
-            ) {
-              const nose = poses[0].keypoints.filter(
-                kpt => kpt.part === "nose"
-              )[0];
-              const eyePosition = {
-                x: nose.position.x / videoWidth,
-                y: nose.position.y / videoHeight
-              };
-              return {
-                type: "SET_STATE",
-                value: {
-                  leftEye: eyePosition,
-                  rightEye: eyePosition
-                }
-              };
-            } else {
-              return {
-                type: "SET_STATE",
-                value: {
-                  leftEye: { x: 0.5, y: 0.5 },
-                  rightEye: { x: 0.5, y: 0.5 }
-                }
-              };
-            }
-          }),
-          express$.map(x => ({
-            type: "EXPRESS",
-            value: { type: x }
-          }))
-        );
-
-        // record data
-        const videoStart$ = sources.VideoRecorder.filter(
-          v => v.type === "START"
-        );
-        const recordedStreams = recordStreams(
-          [
-            {
-              stream: tabletfaceLoaded$,
-              label: "tabletfaceLoaded"
-            },
-            {
-              stream: xs
-                .merge(
-                  xs.of(xs.never()),
-                  askMultipleChoiceFinished$
-                    .filter(x => x === "Let's do this!")
-                    .take(1)
-                    .mapTo(facePoses$),
-                  stopRecording$.take(1)
-                )
-                .flatten(),
-              label: "facePoses"
-            },
-            {
-              stream: xs
-                .merge(xs.of(voiceLevel$), stopRecording$.take(1))
-                .flatten(),
-              label: "voiceLevel"
-            },
-            {
-              stream: askMultipleChoiceFinished$,
-              label: "askMultipleChoiceFinished"
-            },
-            {
-              stream: sayFinished$,
-              label: "sayFinished"
-            },
-            {
-              stream: sinks.setMessage || xs.never(),
-              label: "setMessage"
-            },
-            {
-              stream: sinks.setImage || xs.never(),
-              label: "setImage"
-            },
-            {
-              stream: sinks.askMultipleChoice || xs.never(),
-              label: "askMultipleChoice"
-            },
-            {
-              stream: sinks.say || xs.never(),
-              label: "say"
-            },
-            {
-              stream: sinks.detector || xs.never(),
-              label: "detector"
-            },
-            {
-              stream: sinks.state || xs.never(),
-              label: "state"
-            },
-            {
-              stream: sinks.inputLabel || xs.never(),
-              label: "inputLabel"
-            },
-            {
-              stream: sinks.stateLabel || xs.never(),
-              label: "stateLabel"
-            },
-            {
-              stream: sinks.subStateLabel || xs.never(),
-              label: "stateLabel"
-            },
-            {
-              stream: videoStart$,
-              label: "videoStart"
-            },
-            {
-              stream: tabletFace$.take(1), // skip recoding 'SET_STATE's since they cannot be replayed properly using dataplayer
-              label: "TabletFace"
-            }
-          ],
-          time$
-        );
-        const data$ = xs.combine.apply(null, recordedStreams).map(recorded => {
-          const labels = recorded.map(r => r.label);
-          const combined = recorded.reduce((out, data, i) => {
-            out[labels[i]] = data;
-            return out;
-          }, {});
-          return combined;
-        });
-        dataProxy$.imitate(data$);
-
-        // outputs
-        return {
-          TabletFace: tabletFace$,
-          RobotSpeechbubbleAction: {
-            goal: xs.merge(
-              sinks.setMessage || xs.never(),
-              (sinks.setImage || xs.never()).map(x => ({
-                type: "IMAGE",
-                value: x
-              }))
-            )
-          },
-          HumanSpeechbubbleAction: {
-            goal: sinks.askMultipleChoice || xs.never()
-          },
-          SpeechSynthesisAction: {
-            goal: sinks.say || xs.never()
-          }
-        };
-      },
-      {
-        hidePoseViz: !displayPoseViz,
+  const options = {
+    hidePoseViz: !displayPoseViz,
+    styles: {
+      robotSpeechbubble: {
         styles: {
-          robotSpeechbubble: {
-            styles: {
-              message: {
-                fontSize: "10vmin"
-              }
-            }
+          message: {
+            fontSize: "10vmin"
           }
         }
       }
-    )
+    }
+  };
+  const sinks = withState(
+    withTabletFaceRobotActions(sources => {
+      // Prepare program sinks
+      const time$ = sources.Time.animationFrames()
+        .compose(sources.Time.throttle(100))
+        .map(({ time }) => time);
+      const tabletfaceLoaded$ = sources.TabletFace.events("load");
+      // synchronize sensor inputs
+      const facePoses$ = time$
+        .compose(sampleCombine(sources.PoseDetection.events("poses")))
+        .map(x => x[1]);
+      const voiceLevel$ = time$
+        .compose(sampleCombine(sources.VoiceLevel))
+        .map(x => x[1]);
+      // throttling askMultipleChoiceFinished$ makes buttons to flicker
+      const askMultipleChoiceFinished$ = xs
+        .create({
+          start: listener =>
+            sources.HumanSpeechbubbleAction.result.addListener(listener),
+          stop: () => {}
+        })
+        .map(r => r.result);
+      const sayFinished$ = xs
+        .create({
+          start: listener =>
+            sources.SpeechSynthesisAction.result.addListener(listener),
+          stop: () => {}
+        })
+        .map(r => r.result.text);
+
+      // Create a program
+      const sinks = makeProgram({ Time: sources.Time })({
+        recordedStreams: dataProxy$,
+        tabletfaceLoaded: tabletfaceLoaded$,
+        facePoses: facePoses$,
+        voiceLevel: voiceLevel$,
+        askMultipleChoiceFinished: askMultipleChoiceFinished$,
+        sayFinished: sayFinished$
+      });
+      // process program sinks
+      const followFace$ = sinks.followFace || xs.never();
+      const express$ = sinks.express || xs.never();
+      const tabletFace$ = xs.merge(
+        sources.TabletFace.events("load").mapTo({
+          type: "START_BLINKING",
+          value: { maxInterval: 10000 }
+        }),
+        xs.combine(followFace$, facePoses$).map(([detecting, poses]) => {
+          // "follow face" logic
+          if (
+            !!detecting &&
+            poses.length > 0 &&
+            poses[0].keypoints.filter(kpt => kpt.part === "nose").length > 0
+          ) {
+            const nose = poses[0].keypoints.filter(
+              kpt => kpt.part === "nose"
+            )[0];
+            const eyePosition = {
+              x: nose.position.x / videoWidth,
+              y: nose.position.y / videoHeight
+            };
+            return {
+              type: "SET_STATE",
+              value: {
+                leftEye: eyePosition,
+                rightEye: eyePosition
+              }
+            };
+          } else {
+            return {
+              type: "SET_STATE",
+              value: {
+                leftEye: { x: 0.5, y: 0.5 },
+                rightEye: { x: 0.5, y: 0.5 }
+              }
+            };
+          }
+        }),
+        express$.map(x => ({
+          type: "EXPRESS",
+          value: { type: x }
+        }))
+      );
+
+      // Record data
+      const videoStart$ = sources.VideoRecorder.filter(v => v.type === "START");
+      // TODO: dynamically generate the input
+      // Cache out some data
+      const recordedStreams = recordStreams(
+        [
+          {
+            stream: tabletfaceLoaded$,
+            label: "tabletfaceLoaded"
+          },
+          {
+            // let's not waste memory! do not store data until program-start
+            stream: xs
+              .merge(
+                xs.of(xs.never()),
+                askMultipleChoiceFinished$
+                  .filter(x => x === "Let's do this!")
+                  .take(1)
+                  .mapTo(facePoses$),
+                stopRecording$.take(1)
+              )
+              .flatten(),
+            label: "facePoses"
+          },
+          {
+            // let's not waste memory! do not store data until program-start
+            stream: xs
+              .merge(
+                xs.of(xs.never()),
+                askMultipleChoiceFinished$
+                  .filter(x => x === "Let's do this!")
+                  .take(1)
+                  .mapTo(voiceLevel$),
+                stopRecording$.take(1)
+              )
+              .flatten(),
+            label: "voiceLevel"
+          },
+          {
+            stream: askMultipleChoiceFinished$,
+            label: "askMultipleChoiceFinished"
+          },
+          {
+            stream: sayFinished$,
+            label: "sayFinished"
+          },
+          {
+            stream: sinks.setMessage || xs.never(),
+            label: "setMessage"
+          },
+          {
+            stream: sinks.setImage || xs.never(),
+            label: "setImage"
+          },
+          {
+            stream: sinks.askMultipleChoice || xs.never(),
+            label: "askMultipleChoice"
+          },
+          {
+            stream: sinks.say || xs.never(),
+            label: "say"
+          },
+          {
+            stream: videoStart$,
+            label: "videoStart"
+          },
+          {
+            stream: tabletFace$.take(1), // skip recoding 'SET_STATE's since they cannot be replayed properly using dataplayer
+            label: "TabletFace"
+          }
+        ],
+        time$
+      );
+      const data$ = xs.combine.apply(null, recordedStreams).map(recorded => {
+        const labels = recorded.map(r => r.label);
+        const combined = recorded.reduce((out, data, i) => {
+          out[labels[i]] = data;
+          return out;
+        }, {});
+        return combined;
+      });
+      dataProxy$.imitate(data$);
+
+      // TODOs: cache out outputs
+      // move the options upward
+      return {
+        TabletFace: tabletFace$,
+        RobotSpeechbubbleAction: {
+          goal: xs.merge(
+            sinks.setMessage || xs.never(),
+            (sinks.setImage || xs.never()).map(x => ({
+              type: "IMAGE",
+              value: x
+            }))
+          )
+        },
+        HumanSpeechbubbleAction: {
+          goal: sinks.askMultipleChoice || xs.never()
+        },
+        SpeechSynthesisAction: {
+          goal: sinks.say || xs.never()
+        }
+      };
+    }, options)
   )(sources);
 
   const dataDownloader = DataDownloader(sources, dataProxy$);
