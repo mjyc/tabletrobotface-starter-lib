17d1dcee371b616d07d4bcb87ec079fa33257d32
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-01-25 12:17:27 -0800

    WIP: initial source out

--- src/index.js
+++ src/index.js
@@ -4,27 +4,3 @@ export { default as makeTabletFaceRobot } from "./makeTabletFaceRobot";
 export {
   default as makeAudioAverageFrequencyDriver
 } from "./makeAudioAverageFrequencyDriver";
-
-// module.exports = {
-//   createMain: require("./createMain"),
-//   createDrivers: require("./createDrivers"),
-//   makeAudioAverageFrequencyDriver: require("./makeAudioAverageFrequencyDriver"),
-//   makeTabletFaceRobot: require("./makeTabletFaceRobot")
-// };
-
-// import { run } from "@cycle/run";
-// import createMain from "./createMain";
-// import createDrivers from "./createDrivers";
-
-// const settings = require("./settings.json");
-// // defaults to "dev" setting
-// const { record = true, displayPoseViz = true, hideScroll = false } = settings;
-// if (hideScroll) {
-//   document.body.style.overflow = "hidden";
-// }
-
-// const main = createMain();
-
-// const drivers = createDrivers();
-
-// run(main, drivers);
